72af7f52f846450dadd12f852195a249ac6537d1
GD10 <GD10@HOMEPC>
2016-10-09 00:20:13 +0300

    Add project files.

---
+++ SquareOfStars/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareOfStars
+{
+    class Program
+    {
+
+        
+
+        static void Main(string[] args)
+        {
+
+
+            var COUNTER = int.Parse(Console.ReadLine());
+            const string BLANK = " ";
+            const string STAR = "*";
+            const string SLASH = "/";
+           
+            for (int y = 0; y < COUNTER; y++)
+            {
+                for (int x = 0; x < COUNTER; x++)
+                {
+                    if (y > 0)
+                    {
+                        //Print either star or space
+                        //Blank if inside square
+                    }
+                    else
+                    {
+                        Console.Write(STAR);
+                    }
+                    /*for (int b = 0; b < ; b++)
+                    {
+                        Console.Write(BLANK);
+                    }*/
+                }
+                Console.WriteLine();
+            }
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
